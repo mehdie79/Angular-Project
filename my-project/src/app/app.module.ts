cec9f39c222edc2df65528ae799434bc04eb1863
mehdie <mehdi_esmaeilzadeh@sfu.ca>
2023-02-22 16:52:01 -0800

    Add navbar

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
